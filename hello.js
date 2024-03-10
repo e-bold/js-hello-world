835273702cd73e3c2a927cc2c871ec2e80558c0a
Enkhmunkh Enkhbold <enkhmunkh@att.net>
2024-03-10 16:45:14 -0500

    First batch 31024

--- hello.js
+++ hello.js
@@ -4,19 +4,20 @@ Enter your code on line 7 to print 'Hello, World!'
 to the console (the panel on the right)
 */
 
-console.log("Hello");
+console.log("3 34" ,"Hello");
+console.log('"World"')
 
 /*  
 Next, PLAY! 
 Modify your code on line 7 to try to accomplish the tasks listed below:
 
-   1. Change the message that is printed.
-   2. Figure out what the parentheses do. Will the code work without them?
-   3. Remove one or both quotation marks. Do we need to include both opening and closing quote marks? Is there a difference between using a single or a double quote (' vs. ")?
-   4. Remove the semi-colon, ;.
-   5. Print a number. (Bonus: Print two numbers added together).
+   1. Change the message that is printed.Yes
+   2. Figure out what the parentheses do. Will the code work without them? No
+   3. Remove one or both quotation marks. Do we need to include both opening and closing quote marks? YES Is there a difference between using a single or a double quote (' vs. ")? Seems the same
+   4. Remove the semi-colon, ;.Still runs
+   5. Print a number. (Bonus: Print two numbers added together). YES
    6. Print multiple messages one after the other.
-   7. Print two messages on the same line.
+   7. Print two messages on the same line. console.log("3 34" ,"Hello");
    8. Print a message that contains quote marks, such as Quoth the Raven "Nevermore".
    9. Other. You choose!
 */
